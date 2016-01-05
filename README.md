1ef278c384d171896a5c58335a73c95de636d987
yaoyao <net900621@163.com>
2016-01-05 14:56:49 +0800

    yaoyao

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # reptile
 单页图片爬图
+
+ex:
+node index.js http://www.58pic.com/shiliangtu/16722291.html
---
+++ index.js
@@ -0,0 +1,38 @@
+var http = require('http');    //HTTP协议模块
+var fs = require("fs");
+var fun = function(url){
+    http.get(url, function(res) {
+        // console.log("响应：" + res.statusCode);
+        // console.log("headers: ", res.headers);
+        var _data = '';
+        res.on('data', function (data) {
+            _data += data;
+        });
+        res.on('end', function (data) {
+            var _crawler = _data.match(/src=\"([^[\"|#]*)\"|src=\'([^[\'|#]]*)\'/g).join('').split('src=');
+            _crawler.splice(0, 1);
+            for (var i = 0; i < _crawler.length; i++) {
+                console.log(!_crawler[i].match(/\.[^\.]*/g).pop().match(/jpg|png/g))
+
+                if (!_crawler[i].match(/\.[^\.]*/g).pop().match(/jpg|png/g)) {
+                    return;
+                };
+
+                http.get(_crawler[i].replace(/\'|\"/g, ''), function(_res) {
+                    var _data = '';
+                    _res.setEncoding("binary"); 
+                    _res.on('data', function(data){
+                        _data += data;
+                    }).on('end', function(data){
+                        var _tmp = './pic/' + _res.req.path.split('/').pop();
+                        fs.writeFileSync(_tmp, _data, "binary");
+                    });
+                });
+            };
+        });
+    }).on('error', function(e) {
+        console.log("错误：" + e.message);
+    });
+}
+
+fun(process.argv[2]);
